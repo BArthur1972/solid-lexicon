ea5320ddc52779d6b1c64ed5438524006cbb6f6e
BArthur1972 <82739341+BArthur1972@users.noreply.github.com>
2021-06-20 20:11:05 -0700

    Add files via upload

---
+++ Dictionary.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[44]:
+
+
+import json
+from difflib import get_close_matches
+
+data = json.load(open("Desktop/PYTHON/08 Application 1 Building an Interactive Dictionary/076 data.json"))
+
+def wordbook(word):
+    word = word.lower()
+    if word in data:
+        return data[word]
+    elif len(get_close_matches(word, data.keys())) > 0:
+        recheck = input("Did you mean %s instead? If yes enter Y but if No enter N? " % get_close_matches(word,data.keys())[0])
+        if recheck == "Y":
+            return data[get_close_matches(word,data.keys())[0]]
+        elif recheck == "N":
+            return "The word does not exist, check your spelling."
+        else:
+            return "Check your response again."
+    else:
+        return "The word does not exist, check your spelling."
+ctrl = input("Enter a word: ")
+
+output = wordbook(ctrl)
+
+if type(output) == list:
+    for i in output:
+        print(i)
+else:
+    print(output)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
